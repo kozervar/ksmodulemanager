be4f109ebd21b93880195036566b4d3eac1286cd
Marcin Kozaczyk <kozervar@gmail.com>
2014-12-16 00:18:51 +0100

    Basic run

---
+++ js/IndexContext.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Marcin Kozaczyk on 2014-12-16.
+ */
+'use strict';
+var dateFormat = require('dateformat');
+var newWindow = require('../js/NewWindow');
+
+$(document).ready(function () {
+    var title = document.title;
+    //Definition of the function (non-global, because of the previous line)
+    function setTitle() {
+        var dateString = dateFormat(new Date(), "h:MM:ss");
+        document.title = title + " - " + dateString;
+    }
+
+    //set an interval
+    setInterval(setTitle, 1000);
+
+    // key binding
+    document.onkeydown = function (e) {
+        /// check ctrl + f key
+        if (e.ctrlKey === true && e.keyCode === 112/*F1*/) {
+            e.preventDefault();
+            console.log('Ctrl + F1');
+            newWindow.openNewWindow('editor.html');
+            return false;
+        }
+    }
+
+    // click events
+    $( "#editor" ).click(function() {
+        newWindow.openNewWindow('editor.html');
+    });
+});
---
+++ js/NewWindow.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Marcin Kozaczyk on 2014-12-15.
+ */
+'use strict';
+
+module.exports.openNewWindow = function(toOpen, options){
+    if(toOpen !== undefined) {
+        var opt = options;
+        if(options === undefined) {
+            opt = {
+                "position": "center",
+                "focus": true,
+                "toolbar": false,
+                "frame": true
+            };
+        }
+        return window.open(toOpen, opt);
+    }
+    console.warn('No html file defined to open in new window!');
+};
